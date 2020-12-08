002487dda43f15466bfab6035f8c49752eecd5d4
Aaron Stevens <bheklilr2@gmail.com>
2020-12-08 00:06:12 -0600

    Minor cleanup

--- src/day09.rs
+++ src/day09.rs
@@ -2,6 +2,6 @@ use crate::prelude::*;
 
 pub fn day09() -> Option<Answer<i32>> {
     let mut answer = Answer::new();
-    let values: Vec<i32> = parse_file("inputs/day09.txt")?;
+    let _values: Vec<i32> = parse_file("inputs/day09.txt")?;
     Some(answer)
 }
--- src/day10.rs
+++ src/day10.rs
@@ -2,6 +2,6 @@ use crate::prelude::*;
 
 pub fn day10() -> Option<Answer<i32>> {
     let mut answer = Answer::new();
-    let values: Vec<i32> = parse_file("inputs/day10.txt")?;
+    let _values: Vec<i32> = parse_file("inputs/day10.txt")?;
     Some(answer)
 }
