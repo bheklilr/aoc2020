9af2c44ffc0dcf6b4f7e6122a42c6a800dc9d410
Aaron Stevens <bheklilr2@gmail.com>
2020-12-08 23:52:49 -0600

    I don't like warnings

--- src/day10.rs
+++ src/day10.rs
@@ -3,5 +3,7 @@ use crate::prelude::*;
 pub fn day10() -> R<Answer<i32>> {
     let mut answer = Answer::new();
     let _values: Vec<i32> = parse_file("inputs/day10.txt")?;
+    answer.part1(0);
+    answer.part2(0);
     Ok(answer)
 }
