f8f7d367f39634e95f3c1e03fefa304cc61790d8
Aaron Stevens <bheklilr2@gmail.com>
2020-12-08 23:52:39 -0600

    Implemented day 9

--- src/day09.rs
+++ src/day09.rs
@@ -1,7 +1,39 @@
 use crate::prelude::*;
 
-pub fn day09() -> R<Answer<i32>> {
+pub fn day09(filename: &str) -> R<Answer<usize>> {
     let mut answer = Answer::new();
-    let _values: Vec<i32> = parse_file("inputs/day09.txt")?;
+    let values: Vec<usize> = parse_file(filename)?;
+    let part1 = part1(&values);
+    answer.part1(part1);
+    answer.part2(part2(&values, part1));
     Ok(answer)
+}
+
+fn part1(values: &[usize]) -> usize {
+    let mut preamble: Vec<usize> = values[..25].into();
+    for i in values.iter().skip(25) {
+        let pair = preamble
+            .iter()
+            .flat_map(|x| preamble.iter().map(move |y| (*x, *y)))
+            .filter(|(x, y)| x != y)
+            .find(|(x, y)| x + y == *i);
+        if pair.is_some() {
+            preamble.push(*i);
+            preamble = preamble[1..].into();
+        } else {
+            return *i;
+        }
+    }
+    0
+}
+
+fn part2(values: &[usize], target: usize) -> usize {
+    for i in 2..values.len() - 1 {
+        for window in values.windows(i) {
+            if window.iter().sum::<usize>() == target {
+                return window.iter().min().unwrap() + window.iter().max().unwrap();
+            }
+        }
+    }
+    0
 }
